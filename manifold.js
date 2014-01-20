e4d41620f9ac2a377c7d0de537f5da898b01cbc9
Alex Cruikshank <alex@carbonfive.com>
2014-01-19 23:54:42 -0500

    Types established

--- manifold.js
+++ manifold.js
@@ -1,17 +1,42 @@
-// renderer: face -> [geometry model, three.js, csg.js, stl, etc.]
-// manifold: [0-1] -> [face]
-// vertexGen: [0-1] -> vertex
-
-
 /*
-vertexHandler: vertex ->
-transformer([0-1]) -> 3dTransform
-facer(renderer) -> vertexHandler // vertices have row data
-manifold: indexedVertexHandler -> vertexHandler
-rib: vertexHandler ->
+Step: [0-1]
+StepSink: Step ->
+StepSource: StepSink ->
+  step(n): stepSource // produce n linear steps between 0 and 1 inclusive
+VertexSink: Vertex, Step, Step* ->
+VertexGenerator: VertexSink -> StepSink
+  vertices(vertices) // take the given vertices to create a VertexGenerator
+  vertices(f): (Step -> Vertex) -> VertexGenerator // convert a vertex producer into a vertex generator 
+Transformer: VertexSink -> VertexSink
+  zTranslate(start,end) -> translate vertices along z axis
+  zRotate(start*,end*) -> rotate vertices around z axis
+Facer: FaceSink -> VertexSink
+  face(options): Produce a facer with the given options for tesselating and joining
+Renderer: * -> FaceSink
+  ThreeJSRenderer
+  CSGRenerer
+  STLRenderer
+  NativeRenderer
 */
 
+// cube
+var renderer = ThreeJSRenderer();
 
-XYPointRib([[1,1],[1,-1],[-1,-1],[-1,1]])(
-  linearZ(0,4), )
-) 
+// cube
+step(2)(
+  vertices([[1,1],[-1,1],[-1,-1],[1,-1]])(
+    zTranslate(1)(
+      face({connect:'lr',tesselate:'tb'})( renderer )))))
+
+var geometry = renderer.geometry;
+
+// sphere
+var R = 40, Q = 20, 2PI = 2*Math.Pi, hPI = Math.PI/2;
+function simiCircle(s) {return [R*Math.cos(tween(s,-hPI,hPI)),0,R*Math.sin(tween(s,-hPI,hPI))]}
+
+step(Q)(
+  step(Q)(
+    vertices(semiCircle))(
+      zRotate()(
+        face(connect:'tb', singular:'lr')( renderer ))))
+
